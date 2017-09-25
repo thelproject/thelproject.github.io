bb97ced4d265e1a87b7bf51d5f510873c5ba6f6a
thelproject <l.project@list.ru>
2017-09-25 23:32:48 +0300

    design 3
    design 3

--- Services/Values/galleryValues.js
+++ Services/Values/galleryValues.js
@@ -2,9 +2,9 @@ var app = angular.module('designApp');
 
 app.value('galleryValues', {
     'gallery_photos': [
-        { small: '_017.jpg', full: '017.jpg'},
-        { small: '_018.jpg', full: '018.jpg'},
-        { small: '_019.jpg', full: '019.jpg'},
+        { small: '017s.jpg', full: '017.jpg'},
+        { small: '018s.jpg', full: '018.jpg'},
+        { small: '019s.jpg', full: '019.jpg'},
         { small: '028.jpg', full: '028.jpg'},
         { small: '029.jpg', full: '029.jpg'},
         { small: '030.jpg', full: '030.jpg'},
@@ -30,19 +30,6 @@ app.value('galleryValues', {
         { small: '006.jpg', full: '006.jpg'},
         { small: '009.jpg', full: '009.jpg'},
         { small: '010.jpg', full: '010.jpg'},
-
-
-
-
-
-
-
-
-
-
-
-
-
         { small: '046.jpg', full: '046.jpg'},
         { small: '040.jpg', full: '040.jpg'},
         { small: '041.jpg', full: '041.jpg'},
@@ -50,7 +37,6 @@ app.value('galleryValues', {
         { small: '043.jpg', full: '043.jpg'},
         { small: '044.jpg', full: '044.jpg'},
         { small: '045.jpg', full: '045.jpg'},
-
         { small: '036.jpg', full: '036.jpg'},
         { small: '037.jpg', full: '037.jpg'},
         { small: '038.jpg', full: '038.jpg'},
@@ -62,10 +48,10 @@ app.value('galleryValues', {
         { small: '052.jpg', full: '052.jpg'},
         { small: '053.jpg', full: '053.jpg'},
         { small: '054.jpg', full: '054.jpg'},
-        { small: '_024.jpg', full: '024.jpg'},
-        { small: '_025.jpg', full: '025.jpg'},
-        { small: '_026.jpg', full: '026.jpg'},
-        { small: '_027.jpg', full: '027.jpg'},
+        { small: '024s.jpg', full: '024.jpg'},
+        { small: '025s.jpg', full: '025.jpg'},
+        { small: '026s.jpg', full: '026.jpg'},
+        { small: '027s.jpg', full: '027.jpg'},
         { small: '001.jpg', full: '001.jpg'},
         { small: '002.jpg', full: '002.jpg'}
     ]
